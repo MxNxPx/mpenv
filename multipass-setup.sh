8b205b797ad8154066ff48f7b9535eacb175d51d
MxNxPx <MxNxPx@gmail.com>
2023-01-05 14:44:25 -0500

    tweak cloud init & add option for add'l args for multipass setup

--- multipass-setup.sh
+++ multipass-setup.sh
@@ -4,12 +4,14 @@ IMAGE=22.10
 CPU=8
 MEM=24G
 DISK=99G
+ADD_ARGS=""
+#ADD_ARGS="-vvvv"
 
 ## unset any proxy env vars
 unset PROXY HTTP_PROXY HTTPS_PROXY http_proxy https_proxy
 
 ## launch multipass
-multipass launch $IMAGE --name $NAME --cpus $CPU --mem $MEM --disk $DISK --cloud-init ./cloud-init.yaml
+multipass launch $IMAGE --name $NAME --cpus $CPU --mem $MEM --disk $DISK --cloud-init ./cloud-init.yaml $ADD_ARGS
 sleep 10
 
 ## mount current dir into the multipass instance
