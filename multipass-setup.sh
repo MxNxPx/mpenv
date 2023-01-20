b03f6eb76e95d881a361cd9a59a0697e09cc8d37
MxNxPx <MxNxPx@gmail.com>
2023-01-20 15:01:13 -0500

    change per this msg warning: "--mem" long option will be deprecated in favour of "--memory" in a future release.Please update any scripts, etc.

--- multipass-setup.sh
+++ multipass-setup.sh
@@ -11,7 +11,7 @@ ADD_ARGS=""
 unset PROXY HTTP_PROXY HTTPS_PROXY http_proxy https_proxy
 
 ## launch multipass
-multipass launch $IMAGE --name $NAME --cpus $CPU --mem $MEM --disk $DISK --cloud-init ./cloud-init.yaml $ADD_ARGS
+multipass launch $IMAGE --name $NAME --cpus $CPU --memory $MEM --disk $DISK --cloud-init ./cloud-init.yaml $ADD_ARGS
 sleep 10
 
 ## mount current dir into the multipass instance
